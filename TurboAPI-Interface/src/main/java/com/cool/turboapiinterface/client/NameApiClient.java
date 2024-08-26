d3fc10e04433ac2de9d721275b2cf3ec488044b0
Cool <990299103@qq.com>
2024-08-27 01:43:09 +0800

    feat:"配置API签名认证"

---
+++ TurboAPI-Interface/src/main/java/com/cool/turboapiinterface/client/NameApiClient.java
@@ -0,0 +1,87 @@
+package com.cool.turboapiinterface.client;
+
+import cn.hutool.core.util.RandomUtil;
+import cn.hutool.http.HttpRequest;
+import cn.hutool.http.HttpResponse;
+import cn.hutool.http.HttpUtil;
+import cn.hutool.json.JSONUtil;
+import com.cool.turboapiinterface.model.User;
+import com.cool.turboapiinterface.utils.SignUtils;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 调用第三方接口的客户端
+ *
+ * @author yupi
+ */
+public class NameApiClient {
+    private final String accessKey;
+    private final String secretKey;
+    public NameApiClient(String accessKey, String secretKey) {
+        this.accessKey = accessKey;
+        this.secretKey = secretKey;
+    }
+    // 使用GET方法从服务器获取名称信息
+    public String getNameByGet(String name) {
+        // 可以单独传入http参数，这样参数会自动做URL编码，拼接在URL中
+        HashMap<String, Object> paramMap = new HashMap<>();
+        // 将"name"参数添加到映射中
+        paramMap.put("name", name);
+        // 使用HttpUtil工具发起GET请求，并获取服务器返回的结果
+        String result= HttpUtil.get("http://localhost:8123/api/name/", paramMap);
+        // 打印服务器返回的结果
+        System.out.println(result);
+        // 返回服务器返回的结果
+        return result;
+    }
+
+    // 使用POST方法从服务器获取名称信息
+    public String getNameByPost(@RequestParam String name) {
+        // 可以单独传入http参数，这样参数会自动做URL编码，拼接在URL中
+        HashMap<String, Object> paramMap = new HashMap<>();
+        paramMap.put("name", name);
+        // 使用HttpUtil工具发起POST请求，并获取服务器返回的结果
+        String result= HttpUtil.post("http://localhost:8123/api/name/", paramMap);
+        System.out.println(result);
+        return result;
+    }
+
+    // 使用POST方法向服务器发送User对象，并获取服务器返回的结果
+    public String getUserNameByPost(@RequestBody User user) {
+        // 将User对象转换为JSON字符串
+        String json = JSONUtil.toJsonStr(user);
+        // 使用HttpRequest工具发起POST请求，并获取服务器的响应
+        HttpResponse httpResponse = HttpRequest.post("http://localhost:8123/api/name/user")
+                .addHeaders(getHeaderMap(json))
+                .body(json) // 将JSON字符串设置为请求体
+                .execute(); // 执行请求
+        // 打印服务器返回的状态码
+        System.out.println(httpResponse.getStatus());
+        // 获取服务器返回的结果
+        String result = httpResponse.body();
+        // 打印服务器返回的结果
+        System.out.println(result);
+        // 返回服务器返回的结果
+        return result;
+    }
+    private Map<String,String> getHeaderMap(String body){
+        Map<String,String> hashMap=new HashMap<>();
+        hashMap.put("accessKey",accessKey);
+        // 随机生成一个secretKey,不能直接发送秘钥
+//        hashMap.put("secretKey",secretKey);
+        // 随机生成一个4位数的数字
+        hashMap.put("nonce", RandomUtil.randomNumbers(4));
+        hashMap.put("body",body);
+        //获取当前时间戳
+        hashMap.put("timestamp",String.valueOf(System.currentTimeMillis()/1000));
+        // 生成签名
+//        hashMap.put("sign", SignUtils.genSign(hashMap,secretKey));
+        //获取body，和sk
+        hashMap.put("sign",SignUtils.genSign(body, secretKey));
+        return hashMap;
+    }
+}
--- TurboAPI-Interface/src/main/java/com/cool/turboapiinterface/controller/NameController.java
+++ TurboAPI-Interface/src/main/java/com/cool/turboapiinterface/controller/NameController.java
@@ -1,7 +1,11 @@
 package com.cool.turboapiinterface.controller;
 
 import com.cool.turboapiinterface.model.User;
+import com.cool.turboapiinterface.utils.SignUtils;
 import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+
 
 /**
  * 名称 API
@@ -21,7 +25,32 @@ public class NameController {
     }
 
     @PostMapping("/user")
-    public String getUserNameByPost(@RequestBody User user) {
+    public String getUserNameByPost(@RequestBody User user, HttpServletRequest request) {
+        // 1.拿到这五个我们可以一步一步去做校验,比如 accessKey 我们先去数据库中查一下
+        // 从请求头中获取参数
+        String accessKey = request.getHeader("accessKey");
+        String nonce = request.getHeader("nonce");
+        String timestamp = request.getHeader("timestamp");
+        String sign = request.getHeader("sign");
+        String body = request.getHeader("body");
+        // 不能直接获取秘钥
+        //        String secretKey = request.getHeader("secretKey");
+        // 2.校验权限,这里模拟一下,直接判断 accessKey 是否为"yupi",实际应该查询数据库验证权限
+        if (!accessKey.equals("cool")){
+            throw new RuntimeException("无权限");
+        }
+        // 3.校验一下随机数,因为时间有限,就不带大家再到后端去存储了,后端存储用hashmap或redis都可以
+        // 校验随机数,模拟一下,直接判断nonce是否大于10000
+        if (Long.parseLong(nonce) > 10000) {
+            throw new RuntimeException("无权限");
+        }
+        // 4.校验时间戳与当前时间的差距,交给大家自己实现
+//        if (timestamp) {}
+        String serverSign = SignUtils.genSign(body, "abcdefg");
+        if (!sign.equals(serverSign)){
+            throw new RuntimeException("无权限");
+        }
         return "POST 用户名字是" + user.getUsername();
     }
+
 }
